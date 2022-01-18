1d9571af0d0485e3ba042920540665a632174786
godgodwinter <kakadz.pro@gmail.com>
2022-01-18 08:10:53 +0700

    update layout dashboard

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,43 +1,14 @@
 
-<script>
-    // fungsi fokus di akhir karakter
-(function($){
-    $.fn.focusTextToEnd = function(){
-        this.focus();
-        var $thisVal = this.val();
-        this.val('').val($thisVal);
-        return this;
-    }
-}(jQuery));
-</script>
+    <!-- jQuery -->
+    <script src="{{asset('/')}}assets/gentella/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+   <script src="{{asset('/')}}assets/gentella/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="{{asset('/')}}assets/gentella/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="{{asset('/')}}assets/gentella/vendors/nprogress/nprogress.js"></script>
+    <!-- jQuery custom content scroller -->
+    <script src="{{asset('/')}}assets/gentella/vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
 
-<script type="text/javascript">
-    $(document).ready(function() {
-
-        // In your Javascript (external .js resource or <script> tag)
-            $(document).ready(function() {
-                $('.js-example-basic-single').select2({
-                    // theme: "classic",
-                    // allowClear: true,
-                    width: "resolve"
-                });
-            });
-    });
-   </script>
-
-  <!-- General JS Scripts -->
-  {{-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> --}}
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-  <script src="{{ asset("assets/") }}/js/jquery.uploadPreview.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="{{ asset('/') }}assets/js/stisla.js"></script>
-
-  <!-- JS Libraies -->
-
-  <!-- Template JS File -->
-  <script src="{{ asset('/') }}assets/js/scripts.js"></script>
-  <script src="{{ asset('/') }}assets/js/custom.js"></script>
-
-  <!-- Page Specific JS File -->
+    <!-- Custom Theme Scripts -->
+    <script src="{{asset('/')}}assets/gentella/build/js/custom.min.js"></script>
--- resources/views/includes/sidebar_pemain.blade.php
+++
@@ -1,4 +0,0 @@
-<li {{ $pages == 'dashboard' ? 'class=active' : '' }}><a class="nav-link" href="{{ route('dashboard') }}"><i
-            class="fas fa-home"></i> <span>Dashboard</span></a></li>
-
-<li {{ $pages == 'tahunpenilaian' ? 'class=active' : '' }}><a class="nav-link" href="{{route('pemain.tahunpenilaian')}}"><i class="fas fa-flag-checkered"></i><span>Penilaian </span></a></li>
