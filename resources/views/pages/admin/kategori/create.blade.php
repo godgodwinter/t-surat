edffdea1584a544c5696da63f3d1e357941a8d80
godgodwinter <kakadz.pro@gmail.com>
2022-01-18 12:04:48 +0700

    add curd menu kategori

--- resources/views/pages/admin/kategori/create.blade.php
+++ resources/views/pages/admin/kategori/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.gentella')
 
 @section('title')
-Edit
+Tambah
 @endsection
 
 @push('before-script')
@@ -23,16 +23,28 @@ Edit
             </div>
             <div class="x_content">
                 <br />
-                <form action="{{route('divisi.update',$id->id)}}" method="post" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
-                    @method('put')
-                     @csrf
+                <form action="{{route('kategori.store')}}" method="post" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+                    @csrf
 
                     <div class="item form-group">
-                        <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Nama Lengkap <span class="required">*</span>
+                        <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Nama Kategori <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 ">
-                            <input type="text" id="first-name" required="required" class="form-control @error('divisi') is-invalid @enderror" name="nama" value="{{old('nama') ? old('nama') : $id->nama}}">
+                            <input type="text"  required="required" class="form-control @error('nama') is-invalid @enderror" name="nama">
                             @error('nama')<div class="invalid-feedback"> {{$message}}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="item form-group">
+                        <label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Prefix <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 ">
+                            <select id="heard" class="form-control @error('prefix') is-invalid @enderror" required name="prefix">
+                                <option value="">Pilih Prefix ...</option>
+                                <option value="suratmasuk">Surat Masuk</option>
+                                <option value="suratkeluar">Surat Keluar</option>
+                            </select>
+                            @error('prefix')<div class="invalid-feedback"> {{$message}}</div>
                             @enderror
                         </div>
                     </div>
